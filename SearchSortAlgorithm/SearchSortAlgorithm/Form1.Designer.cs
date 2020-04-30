0fc9357c41e9003cb9ced5bdbcd8e8010f56923e
FourthEarleOfSandwich <63482043+FourthEarleOfSandwich@users.noreply.github.com>
2020-04-30 10:05:11 -0400

    Add files via upload

---
+++ SearchSortAlgorithm/SearchSortAlgorithm/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SearchSortAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_MergeSort = new System.Windows.Forms.Button();
+            this.label_Output = new System.Windows.Forms.Label();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.button_InsertionSort = new System.Windows.Forms.Button();
+            this.button_SelectionSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_MergeSort
+            // 
+            this.button_MergeSort.Location = new System.Drawing.Point(62, 106);
+            this.button_MergeSort.Name = "button_MergeSort";
+            this.button_MergeSort.Size = new System.Drawing.Size(190, 65);
+            this.button_MergeSort.TabIndex = 0;
+            this.button_MergeSort.Text = "Merge Sort";
+            this.button_MergeSort.UseVisualStyleBackColor = true;
+            this.button_MergeSort.Click += new System.EventHandler(this.button_MergeSort_Click);
+            // 
+            // label_Output
+            // 
+            this.label_Output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_Output.Location = new System.Drawing.Point(62, 210);
+            this.label_Output.Name = "label_Output";
+            this.label_Output.Size = new System.Drawing.Size(515, 160);
+            this.label_Output.TabIndex = 1;
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(62, 25);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(126, 65);
+            this.button_Search.TabIndex = 2;
+            this.button_Search.Text = "Binary Search";
+            this.button_Search.UseVisualStyleBackColor = true;
+            this.button_Search.Click += new System.EventHandler(this.button_Search_Click);
+            // 
+            // button_InsertionSort
+            // 
+            this.button_InsertionSort.Location = new System.Drawing.Point(359, 36);
+            this.button_InsertionSort.Name = "button_InsertionSort";
+            this.button_InsertionSort.Size = new System.Drawing.Size(141, 64);
+            this.button_InsertionSort.TabIndex = 3;
+            this.button_InsertionSort.Text = "InsertionSort";
+            this.button_InsertionSort.UseVisualStyleBackColor = true;
+            this.button_InsertionSort.Click += new System.EventHandler(this.button_InsertionSort_Click);
+            // 
+            // button_SelectionSort
+            // 
+            this.button_SelectionSort.Location = new System.Drawing.Point(345, 127);
+            this.button_SelectionSort.Name = "button_SelectionSort";
+            this.button_SelectionSort.Size = new System.Drawing.Size(176, 44);
+            this.button_SelectionSort.TabIndex = 4;
+            this.button_SelectionSort.Text = "Selection Sort";
+            this.button_SelectionSort.UseVisualStyleBackColor = true;
+            this.button_SelectionSort.Click += new System.EventHandler(this.button_SelectionSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 659);
+            this.Controls.Add(this.button_SelectionSort);
+            this.Controls.Add(this.button_InsertionSort);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.label_Output);
+            this.Controls.Add(this.button_MergeSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_MergeSort;
+        private System.Windows.Forms.Label label_Output;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.Button button_InsertionSort;
+        private System.Windows.Forms.Button button_SelectionSort;
+    }
+}
+
---
+++ SearchSortAlgorithm/SearchSortAlgorithm/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchSortAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        int[] IntArray;
+        const int MAX = 10;
+        int[] SortedArray = { 1, 2, 3, 4, 5 };
+            
+    public Form1()
+        {
+            InitializeComponent();
+            
+            //int start2 = random.Next(0, 100);
+            //Generate();
+
+        }
+
+        private void Generate()
+        {
+            Random random = new Random();
+            IntArray = new int[MAX];
+
+            for (int ii = 0; ii < MAX; ii++)
+            {
+                IntArray[ii] = random.Next(0, 100);
+            }
+        }
+
+        //Binary Search
+        public int BinarySearch(int[] sortedarray, int selectedNumber)
+        {
+            int low = 0;
+            int high = sortedarray.Length - 1;
+            while(true)
+            {
+                
+                int middle = (low + high) / 2;
+                if(sortedarray[middle] == selectedNumber)
+                {
+                    return middle;
+                }
+                else if(sortedarray[middle] < selectedNumber)
+                {
+                    low = middle + 1;
+                }
+                else if(sortedarray[middle] > selectedNumber)
+                {
+                    high = middle - 1;
+                }
+                if(high < low)
+                {
+                    return -1;
+                }
+            }
+        }
+
+        public void MergeSort(int[] unsortedarray, int startindex, int endindex)
+        {
+            if (startindex == endindex)
+            {
+                return;
+            }
+            int middle = (startindex + endindex) / 2;
+            MergeSort(unsortedarray, startindex, middle);
+            MergeSort(unsortedarray, middle + 1, endindex);
+            Merge(unsortedarray, startindex, endindex, middle);
+        }
+
+        private static void Merge(int[] items, int startindex, int endindex, int middle)
+        {
+            int temp;
+
+            for (int ii = middle + 1; ii <= endindex; ii++)
+            {
+
+                for (int jj = ii - 1; jj >= startindex; jj--)
+                {
+                    if (items[jj] > items[jj + 1])
+                    {
+                        temp = items[jj];
+                        items[jj] = items[jj + 1];
+                        items[jj + 1] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+        }
+         private static void InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            
+        }
+        private static void SelectionSort(int[] data)
+        {
+            int temp;
+            int smallest;
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                smallest = i;
+
+                for (int index = i + 1; index < data.Length; index++)
+                {
+                    if (data[index] < data[smallest])
+                    {
+                        smallest = index;
+                    }
+                }
+                
+                temp = data[i];
+                data[i] = data[smallest];
+                data[smallest] = temp;
+            }
+
+        }
+
+        private void button_MergeSort_Click(object sender, EventArgs e)
+        {
+            Generate();
+            label_Output.Text = "Unsorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+            MergeSort(IntArray, 0, MAX-1);
+            label_Output.Text += "Sorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+            
+        }
+
+        private void button_Search_Click(object sender, EventArgs e)
+        {
+            int value = 5;
+            label_Output.Text = "Number " + value.ToString() + " is at index ";
+           label_Output.Text += BinarySearch(SortedArray, value).ToString();
+        }
+
+        private void button_SelectionSort_Click(object sender, EventArgs e)
+        {
+            Generate();
+            label_Output.Text = "Unsorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+            SelectionSort(IntArray);
+            label_Output.Text += "Sorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+
+        }
+        private void button_InsertionSort_Click(object sender, EventArgs e)
+        {
+            Generate();
+            label_Output.Text = "Unsorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+            InsertionSort(IntArray);
+            label_Output.Text += "Sorted ";
+            foreach (int j in IntArray)
+            {
+                label_Output.Text += j.ToString() + ", ";
+            }
+
+        }
+    }
+
+        
+}
+    
+
